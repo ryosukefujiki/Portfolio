024b415a9a7f6cabdfd1d2fee423e03de12848a0
daifuku <fis10sai@i.softbank.jp>
2019-02-28 15:44:12 +0900

    Update the title tag
    titleタグのテキストをRyosuke Fujiki Portfolioに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'ryosukefujikiportfolio',
+    title: 'Ryosuke Fujiki Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
