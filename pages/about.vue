33f98c5951da898b29fb9df10c216928f0f0e281
daifuku <fis10sai@i.softbank.jp>
2019-02-28 14:43:05 +0900

    Add the vuex file

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>Aboutページテスト</h1>
+      <p>count={{count}}</p>
+      <button @click="addCount">カウントアップ</button>
+    </div>
+  </section>
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex'
+
+export default {
+  computed: {
+    count () { return this.$store.state.counter.count }
+  },
+  methods: {
+    addCount (e) {
+      this.$store.commit('counter/add')
+    }
+  }
+}
+</script>
