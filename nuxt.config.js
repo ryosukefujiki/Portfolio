1cbdee347e4dc9c9dec93ac48343703a03e60c87
daifuku <fis10sai@i.softbank.jp>
2019-02-28 20:31:30 +0900

    Add the google web font
    googleのWebフォントを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,11 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans:700i'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/earlyaccess/notosansjapanese.css'},
     ]
+    
   },
   /*
   ** Customize the progress bar color
