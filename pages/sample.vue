b78ddf95c7431e37509df2541dbe559a4d89a1ca
daifuku <fis10sai@i.softbank.jp>
2019-03-04 17:01:25 +0900

    change the file name of vuex sample file
    vuexのサンプルページのファイルをリネーム

---
+++ pages/sample.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>Aboutページテスト</h1>
+      <p>count={{count}}</p>
+      <button @click="addCount">カウントアップ</button>
+    </div>
+  </section>
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex'
+
+export default {
+  computed: {
+    count () { return this.$store.state.counter.count }
+  },
+  methods: {
+    addCount (e) {
+      this.$store.commit('counter/add')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
