9ed02924c056f296b671cf486d738717d6ee9ef7
daifuku <fis10sai@i.softbank.jp>
2019-04-23 23:07:22 +0900

    Add the TheFirstview.vue

---
+++ components/TheFirstview.vue
@@ -0,0 +1,21 @@
+<template>
+  <BarLoader></BarLoader>
+</template>
+
+<script>
+import { BarLoader } from '@saeris/vue-spinners'
+
+export default {
+  components: {
+    BarLoader
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+
