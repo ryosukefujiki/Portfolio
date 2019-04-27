26500df01836dd1c61e691ee5d0a5c43bb074286
daifuku <fis10sai@i.softbank.jp>
2019-04-28 03:34:38 +0900

    Add the profile img hover animation

--- components/TheAbout/TheProfileImg.vue
+++ components/TheAbout/TheProfileImg.vue
@@ -1,52 +1,171 @@
 <template>
-    <div class="TheProfileImg glitch glitch--style-1">
-        <img :src="profileImg" alt="" class="TheAbout_ProfileImg glitch__img">
+  <div class="TheProfileImg" @mouseenter="profileBackImgOpen" @mouseleave="profileBackImgHidden">
+    <div>
+      <div class="TheAbout_ProfileTransition"></div>
+      <img :src="profileBackImg" alt class="TheAbout_ProfileBackImg">
+      <img :src="profileImg" alt class="TheAbout_ProfileImg">
     </div>
+  </div>
 </template>
 
 <script>
+import { TweenMax, Elastic, Expo, Back } from "gsap";
+import { mapGetters } from "vuex";
+
 export default {
-    data () {
+  data() {
     return {
-      profileImg: '/about/profile3.png',
-      profileBackImg: '/about/profile_back.png',
+      profileImg: "/about/profile3.png",
+      profileBackImg: "/about/profile_back.png"
+    };
+  },
+  computed: {
+    ...mapGetters({
+      hiddened: "about/profileHiddened",
+      opened: "about/profileOpened"
+    })
+  },
+  methods: {
+    profileBackImgOpen: function() {
+      console.log("open");
+      this.$store.commit("about/profileOpen");
+    },
+    profileBackImgHidden: function() {
+      console.log("close");
+      this.$store.commit("about/profileHidden");
     }
   },
-}
+  watch: {
+    async opened(val) {
+      requestAnimationFrame(() => {
+        TweenMax.to(".TheAbout_ProfileTransition", 0.55, {
+          left: "0px",
+          ease: Expo.easeOut,
+          delay: 0.1,
+          startAt: {
+            left: "-480px"
+          }
+        });
+      });
+      await this.$delay(400);
+      requestAnimationFrame(() => {
+        TweenMax.to(".TheAbout_ProfileBackImg", 0, {
+          opacity: 1,
+          ease: Expo.easeOut,
+          startAt: {
+            opacity: 0
+          }
+        });
+      });
+      await this.$delay(400);
+      requestAnimationFrame(() => {
+        TweenMax.to(".TheAbout_ProfileTransition", 0.55, {
+          left: "480px",
+          ease: Expo.easeIn,
+          startAt: {
+            left: "0px"
+          }
+        });
+      });
+    },
+    async hiddened(val) {
+      requestAnimationFrame(() => {
+        TweenMax.to(".TheAbout_ProfileTransition", 0.55, {
+          left: "0px",
+          ease: Expo.easeOut,
+          delay: 0.1,
+          startAt: {
+            left: "-480px"
+          }
+        });
+      });
+      await this.$delay(700);
+      requestAnimationFrame(() => {
+        TweenMax.to(".TheAbout_ProfileBackImg", 0, {
+          opacity: 0,
+          ease: Expo.easeOut,
+          startAt: {
+            opacity: 1
+          }
+        });
+      });
+      await this.$delay(400);
+      requestAnimationFrame(() => {
+        TweenMax.to(".TheAbout_ProfileTransition", 0.55, {
+          left: "480px",
+          ease: Expo.easeIn,
+          startAt: {
+            left: "0px"
+          }
+        });
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-.TheAbout_ProfileImg{
+.TheProfileImg {
+  overflow: hidden;
+}
+.TheProfileImg div {
+  position: relative;
+  // overflow: hidden;
+}
+.TheAbout_ProfileImg {
   width: 480px;
   height: 320px;
-  box-shadow: 0px 0px 4px 0px rgba(155,155,155,0.2);
+  box-shadow: 0px 0px 4px 0px rgba(155, 155, 155, 0.2);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
 }
-
+.TheAbout_ProfileBackImg {
+  width: 480px;
+  height: 320px;
+  box-shadow: 0px 0px 4px 0px rgba(155, 155, 155, 0.2);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  opacity: 0;
+}
+.TheAbout_ProfileTransition {
+  width: 480px;
+  height: 320px;
+  position: absolute;
+  top: 0;
+  left: -480px;
+  z-index: 3;
+  background: #222222;
+  opacity: 1;
+}
 
 @media screen and (max-width: 1024px) {
-    /* 1024pxまでの幅の場合に適応される */
-    .TheAbout_ProfileImg{
-        width: 360px;
-        height: 240px;
-    }
+  /* 1024pxまでの幅の場合に適応される */
+  .TheAbout_ProfileImg {
+    width: 360px;
+    height: 240px;
+  }
 }
 @media screen and (max-width: 768px) {
-    .TheAbout_ProfileImg{
-        width: 300px;
-        height: 200px;
-    }
+  .TheAbout_ProfileImg {
+    width: 300px;
+    height: 200px;
+  }
 }
 @media screen and (max-width: 480px) {
-    .TheAbout_ProfileImg{
-        width: 320px;
-        height: 212px;
-    }
+  .TheAbout_ProfileImg {
+    width: 320px;
+    height: 212px;
+  }
 }
 @media screen and (max-width: 320px) {
-    .TheAbout_ProfileImg{
-        width: 300px;
-        height: 200px;
-    }
+  .TheAbout_ProfileImg {
+    width: 300px;
+    height: 200px;
+  }
 }
 </style>
 
--- store/about.js
+++ store/about.js
@@ -1,11 +1,15 @@
 export const state = () => ({
     aboutEntered: false,
     aboutMoved: false,
+    profileOpened: false,
+    profileHiddened: false,
   })
   
   export const getters = {
     aboutEntered: state => state.aboutEntered,
     aboutMoved: state => state.aboutMoved,
+    profileOpened: state => state.profileOpened,
+    profileHiddened: state => state.profileHiddened,
   }
   // 上記はアロー関数　stateを引数にとって、state.enteredの値をgetterからアクセスできるようにしている
   
@@ -15,6 +19,12 @@ export const state = () => ({
     },
     aboutMove (state) {
       state.aboutMoved= !state.aboutMoved
+    },
+    profileOpen (state) {
+      state.profileOpened= !state.profileOpened
+    },
+    profileHidden (state) {
+      state.profileHiddened= !state.profileHiddened
     },
   }
   
