2193e272af860482da3b4f33f91bf12c62c9b3bc
daifuku <fis10sai@i.softbank.jp>
2019-04-29 19:26:59 +0900

    Update the background component for responsive

--- components/TheHome/TheBackground.vue
+++ components/TheHome/TheBackground.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-    
+
 }
 </script>
 
@@ -19,7 +19,49 @@ export default {
     position: absolute;
     top: -25%;
     left: -50%;
-    // z-index: -1;
+}
+@media screen and (max-width: 1024px) {
+    .TheBackground{
+    background: red;
+    width: 150%;
+    height: 150%;
+    position: absolute;
+    top: -25%;
+    left: -25%;
+}
+}
+
+@media screen and (max-width: 768px) {
+    .TheBackground{
+    background: red;
+    width: 100%;
+    height: 150%;
+    position: absolute;
+    top: -10%;
+    left: 0;
+    }
+}
+
+@media screen and (max-width: 480px) {
+    .TheBackground{
+    background: red;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: -15%;
+    left: 0;
+    }
+}
+
+@media screen and (max-width: 320px) {
+    .TheBackground{
+    background: red;
+    width: 100%;
+    height: 80%;
+    position: absolute;
+    top: -15%;
+    left: 0;
+    }
 }
 </style>
 
