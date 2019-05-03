ffed88ad65bb0140943129161236f5f82d3d207c
daifuku <fis10sai@i.softbank.jp>
2019-05-04 03:33:53 +0900

    Fix the bug in the sorry.vue

--- pages/sorry.vue
+++ pages/sorry.vue
@@ -1,44 +1,9 @@
-<template lang="pug">
-  .sorry(
-    ref='sorry'
-  )
-    .tbc ご使用のブラウザには対応しておりません。ごめんなさいっっっ( ´ ; ω ; ` )
+<template>
+  <div class="sorry">
+    <h2>Sorry...🙇‍♂️お使いのブラウザに対応しておりません</h2>
+  </div>
 </template>
 
-<script>
-import {TweenMax, Expo} from 'gsap'
 
-export default {
-  mounted () {
-    requestAnimationFrame(() => {
-      TweenMax.to(this.$refs.sorry, 2, {
-        opacity: 1,
-        y: 0,
-        startAt: {
-          y: '30px'
-        },
-        ease: Expo.easeInOut
-      })
-    })
-  },
-  head: {
-    title: 'Sorry...'
-  },
-  layout: 'poster'
-}
-</script>
-
-<style lang="stylus" scoped>
-.sorry
-  display table
-  padding 0 30px
-  width 100%
-  height 100%
-  text-align center
-  color #555
-  font-size 18px
-  font-weight bold
-  opacity 0
-</style>
 
 
